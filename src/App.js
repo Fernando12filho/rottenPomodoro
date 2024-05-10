81bdea3797c7721982b22899981585d69a816e47
Fernando Melo <fgmelofilho@icloud.com>
2024-05-10 19:10:53 -0400

    added a separate div inside body for timer, added chakra, added comments to settings, added pomodoro and break inputs

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import TimerSec from "./components/Timer/TimerSec";
 
 
 function App() {
+  
   return (
-    <div className="App">
-      <div className="body">
+    <div className="body">
+      <div className="timer">
         <TimerSec />
       </div>
     </div>
--- src/components/Settings/Setting.js
+++ src/components/Settings/Setting.js
@@ -1,23 +1,40 @@
-const style = {
-  settings: {
-    backgroundColor: "red",
-    position: "absolute",
-    height: "160px",
-    width: "250px",
-    top: "480px",
-    borderRadius: "30px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-};
+import { useState } from "react";
+
+/*
+Settings need: 
+    Set Pomodoro time and break time
+    Store props
+    Pass it down clock
+*/
+
 
 function Settings() {
+    const [pomodoro, setPomodoro] = useState();
+
   return (
     <div className="settings" style={style.settings}>
-      <h1>Settings</h1>
+      <p>Pomodoro</p>
+      <input></input>
+      <p>Break</p>
+      <input></input>
     </div>
   );
 }
+
+const style = {
+    settings: {
+      backgroundColor: "red",
+      position: "absolute",
+      height: "160px",
+      width: "250px",
+      top: "63%",
+      borderRadius: "30px",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "white",
+    },
+  };
 
 export default Settings;
